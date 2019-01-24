ccf6a98348cbffc0984ba3188c5e4c1b394b590e
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-01-24 15:16:26 +0545

    remove duplicate nav

--- index.php
+++ index.php
@@ -3,16 +3,6 @@
 get_header();
 
 ?>
-<center>
-<div class="nav">
-    <ul>
-        <li> <a href="#">Home </a></li>
-        <li> <a href="#">About </a></li>
-        <li> <a href="#">Blog </a></li>
-        <li> <a href="#">Contact </a></li>
-    </ul>
-</div>
-</center>
 
 <?php 
 while(have_posts()){
