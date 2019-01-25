3e9080a6e73ade4c77d155ed54cc9de5b3b8535a
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-01-25 16:05:52 +0545

    New Navigation Menu

--- header.php
+++ header.php
@@ -6,15 +6,37 @@
 </head>
 <body>
 
-<!----------#########Start of the navigation menu ------->
-<center>
-<div class="nav">
-    <ul>
-        <li> <a href="<?php echo site_url(); ?>">Home </a></li>
-        <li> <a href="<?php echo site_url('about'); ?>">About </a></li>
-        <li> <a href="<?php echo site_url('blog'); ?>">Blog </a></li>
-        <li> <a href="<?php echo site_url('contact'); ?>">Contact </a></li>
-    </ul>
-</div>
-</center>
-<!--------------######end of the navigation menu ----->
+<!-- MENU -->
+<section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                    <!-- lOGO TEXT HERE -->
+                    <a href="#" class="navbar-brand">Known</a>
+               </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-nav-first">
+                         <li><a href="#top" class="smoothScroll">Home</a></li>
+                         <li><a href="#about" class="smoothScroll">About</a></li>
+                         <li><a href="#team" class="smoothScroll">Our Teachers</a></li>
+                         <li><a href="#courses" class="smoothScroll">Courses</a></li>
+                         <li><a href="#testimonial" class="smoothScroll">Reviews</a></li>
+                         <li><a href="#contact" class="smoothScroll">Contact</a></li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                         <li><a href="#"><i class="fa fa-phone"></i> +65 2244 1100</a></li>
+                    </ul>
+               </div>
+
+          </div>
+     </section>
+
